634c0c9c4c2809e8d19aa9113781b5514132c268
dreamjser <dreamjser@gmail.com>
2024-03-21 00:20:17 +0800

    feat: token存储

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module'
@@ -15,6 +16,10 @@ import { User } from './user/user.entity';
       database: 'answer',
       entities: [User],
       synchronize: true,
+    }),
+    CacheModule.register({
+      ttl: 3600* 1000,
+      isGlobal: true,
     }),
     UserModule,
   ],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Inject, Post, Get, Body } from '@nestjs/common';
+import { Controller, Inject, Post, Get, Body, Query } from '@nestjs/common';
 import { Cache } from 'cache-manager'
 import { CACHE_MANAGER } from '@nestjs/cache-manager'
 import { md5 } from 'js-md5'
@@ -14,7 +14,7 @@ export class UserController {
 
   @Post('login')
   async query(@Body() body): Promise<Result> {
-    const tokenStr: string = `${body.name}${body.pwd}`
+    const tokenStr: string = `${body.name}${body.pwd}${+new Date()}`
     const token = md5(tokenStr)
     const str = await this.cacheManager.get(token);
     let data = null
@@ -23,11 +23,20 @@ export class UserController {
       data = str
     }else{
       data = await this.userService.findUserById(body.name, body.pwd)
-      await this.cacheManager.set(token, data, 3600);
+      await this.cacheManager.set(token, data);
     }
 
     data.token = token
 
+    return {
+      errorCode: 0,
+      errorMsg: '登录成功',
+      data,
+    }
+  }
+  @Get('getUserInfo')
+  async queryUserInfo(@Query() query): Promise<Result>{
+    const data:any = await this.cacheManager.get(query.token);
     return {
       errorCode: 0,
       errorMsg: '登录成功',
